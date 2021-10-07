e25cb13a98e5233758066a28c6febba1470848ec
Sanal KV <sanal@ageyetech.com>
2021-10-07 21:55:14 +0530

    bug fixes

--- lib/ui/coin_details/coin_details_view.dart
+++ lib/ui/coin_details/coin_details_view.dart
@@ -54,95 +54,101 @@ class CoinDetailsView extends StatelessWidget {
               SizedBox(
                 height: 16,
               ),
-              Align(
-                  alignment: Alignment.centerRight,
-                  child: Text(
-                    'From ${model.timeFrom} to ${model.timeTo}',
-                    style: TextStyle(fontSize: 9),
-                  )),
-              SizedBox(
-                height: 8,
-              ),
               Expanded(
                 child: ResponseHandler(
                   isBusy: model.isBusy,
                   error: model.modelError,
                   hasError: model.hasError,
-                  child: LineChart(
-                    LineChartData(
-                      gridData: FlGridData(drawHorizontalLine: false, drawVerticalLine: false),
-                      lineBarsData: [
-                        LineChartBarData(
-                          belowBarData: BarAreaData(
-                            show: true,
-                            gradientFrom: Offset(1, 0),
-                            gradientTo: Offset(1, 1),
-                            gradientColorStops: [0.1, 0.8],
-                            spotsLine: BarAreaSpotsLine(
+                  child: Column(
+                    children: [
+                      Align(
+                          alignment: Alignment.centerRight,
+                          child: Text(
+                            'From ${model.timeFrom} to ${model.timeTo}',
+                            style: TextStyle(fontSize: 9),
+                          )),
+                      SizedBox(
+                        height: 8,
+                      ),
+                      Expanded(
+                        child: LineChart(
+                          LineChartData(
+                            gridData: FlGridData(drawHorizontalLine: false, drawVerticalLine: false),
+                            lineBarsData: [
+                              LineChartBarData(
+                                belowBarData: BarAreaData(
+                                  show: true,
+                                  gradientFrom: Offset(1, 0),
+                                  gradientTo: Offset(1, 1),
+                                  gradientColorStops: [0.1, 0.8],
+                                  spotsLine: BarAreaSpotsLine(
+                                    show: true,
+                                    flLineStyle: FlLine(
+                                      color: Colors.grey,
+                                      dashArray: [1, 3],
+                                      strokeWidth: 1,
+                                    ),
+                                  ),
+                                  colors: [
+                                    Colors.blue.withOpacity(0.8),
+                                    Colors.blue.withOpacity(0.05),
+                                  ],
+                                ),
+                                spots: model.coinHistory?.priceData
+                                    ?.map(
+                                      (priceData) => FlSpot(priceData.time?.toDouble() ?? 0, priceData.price ?? 0),
+                                    )
+                                    .toList(),
+                                dotData: FlDotData(show: false),
+                              )
+                            ],
+                            titlesData: FlTitlesData(
                               show: true,
-                              flLineStyle: FlLine(
-                                color: Colors.grey,
-                                dashArray: [1, 3],
-                                strokeWidth: 1,
+                              topTitles: SideTitles(showTitles: false),
+                              rightTitles: SideTitles(showTitles: false),
+                              leftTitles: SideTitles(
+                                showTitles: true,
+                                reservedSize: 40,
+                                // checkToShowTitle: (_, __, ____, _____, value) => true,
+                              ),
+                              bottomTitles: SideTitles(
+                                showTitles: true,
+                                margin: 5,
+                                interval: 5,
+                                checkToShowTitle: (_, __, ____, _____, value) {
+                                  return model.coinHistory?.priceData?.any((priceData) => priceData.time == value) ?? false;
+                                },
+                                getTextStyles: (_, __) => TextStyle(fontSize: 8),
+                                getTitles: (value) {
+                                  return model.getDateFromEpoch(value);
+                                },
                               ),
                             ),
-                            colors: [
-                              Colors.blue.withOpacity(0.8),
-                              Colors.blue.withOpacity(0.05),
-                            ],
-                          ),
-                          spots: model.coinHistory?.priceData
-                              ?.map(
-                                (priceData) => FlSpot(priceData.time?.toDouble() ?? 0, priceData.price ?? 0),
-                              )
-                              .toList(),
-                          dotData: FlDotData(show: false),
-                        )
-                      ],
-                      titlesData: FlTitlesData(
-                        show: true,
-                        topTitles: SideTitles(showTitles: false),
-                        rightTitles: SideTitles(showTitles: false),
-                        leftTitles: SideTitles(
-                          showTitles: true,
-                          reservedSize: 40,
-                          // checkToShowTitle: (_, __, ____, _____, value) => true,
-                        ),
-                        bottomTitles: SideTitles(
-                          showTitles: true,
-                          margin: 5,
-                          interval: 5,
-                          checkToShowTitle: (_, __, ____, _____, value) {
-                            return model.coinHistory?.priceData?.any((priceData) => priceData.time == value) ?? false;
-                          },
-                          getTextStyles: (_, __) => TextStyle(fontSize: 8),
-                          getTitles: (value) {
-                            return model.getDateFromEpoch(value);
-                          },
-                        ),
-                      ),
-                      borderData: FlBorderData(
-                        show: true,
-                        border: Border(
-                          bottom: BorderSide(
-                            color: Colors.black,
-                            width: 0.5,
-                          ),
-                          top: BorderSide(
-                            color: Colors.black,
-                            width: 0.5,
-                          ),
-                          left: BorderSide(
-                            color: Colors.black,
-                            width: 0.5,
-                          ),
-                          right: BorderSide(
-                            color: Colors.black,
-                            width: 0.5,
+                            borderData: FlBorderData(
+                              show: true,
+                              border: Border(
+                                bottom: BorderSide(
+                                  color: Colors.black,
+                                  width: 0.5,
+                                ),
+                                top: BorderSide(
+                                  color: Colors.black,
+                                  width: 0.5,
+                                ),
+                                left: BorderSide(
+                                  color: Colors.black,
+                                  width: 0.5,
+                                ),
+                                right: BorderSide(
+                                  color: Colors.black,
+                                  width: 0.5,
+                                ),
+                              ),
+                            ),
                           ),
                         ),
                       ),
-                    ),
+                    ],
                   ),
                 ),
               ),
